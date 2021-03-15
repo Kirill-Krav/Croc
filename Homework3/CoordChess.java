0f238d374f354b66cff90d8e226223b64e4a11a8
Kirill-Krav <79889002+Kirill-Krav@users.noreply.github.com>
2021-03-15 20:05:40 +0800

    Add files via upload

---
+++ Homework3/CoordChess.java
@@ -0,0 +1,42 @@
+package Homework3;
+
+public class CoordChess{
+
+    int x;
+    int y;
+
+    CoordChess(int x, int y) throws IllegalAccessException {
+        if(x < 0 || x > 7 || y < 0 || y > 7){
+            throw new IllegalAccessException("неправильные координаты");
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) throws IllegalAccessException {
+        if(x < 0 || x > 7){
+            throw new IllegalAccessException("неправильная координата");
+        }
+        this.x = x;
+    }
+
+    public void setY(int y) throws IllegalAccessException {
+        if(y < 0 || y > 7){
+            throw new IllegalAccessException("неправильная координата");
+        }
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return new String(String.valueOf((char)(x + 'a')) + String.valueOf((char)(y + '1')));
+    }
+}
